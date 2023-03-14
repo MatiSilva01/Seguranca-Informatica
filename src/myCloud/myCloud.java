6500dc0da6c1ad884db7a80d570a4bd6ec5a4992
Matilde Silva <matildeferreirasilva@gmail.com>
2023-03-14 17:23:58 +0000

    inicio

--- src/myCloud/myCloud.java
+++ src/myCloud/myCloud.java
@@ -1,5 +1,96 @@
 package myCloud;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class myCloud {
+
+    public static void main(String[] args) throws UnknownHostException, IOException, ClassNotFoundException {
+        if (args.length < 3 || args[0] != "-a") { // ficheiros?
+            System.err.println("Usage: java myCloud -a <server address> -c|-s|-e|-g <filename(s)>");
+            System.exit(1);
+        }
+        String[] host = null;
+        String operation;
+        List<String> filelist;
+        int i = 0;
+        while (i < args.length) {
+            if (args[i].equals("-a")) {
+                host = args[i + 1].split(":");
+
+            } else if (args[i].equals("-c")) {
+                operation = args[i];
+                filelist = new ArrayList<>();
+                for (int j = 1; i + j < args.length; j++) {
+                    String f = args[i + j];
+                    filelist.add(f);
+                }
+                ;
+            } else if (args[i].equals("-s")) {
+                operation = args[i];
+                filelist = new ArrayList<>();
+                for (int j = 1; i + j < args.length; j++) {
+                    String f = args[i + j];
+                    filelist.add(f);
+                }
+                ;
+            } else if (args[i].equals("-e")) {
+                operation = args[i];
+                filelist = new ArrayList<>();
+                for (int j = 1; i + j < args.length; j++) {
+                    String f = args[i + j];
+                    filelist.add(f);
+                }
+                ;
+            } else if (args[i].equals("-g")) {
+                operation = args[i];
+                filelist = new ArrayList<>();
+                for (int j = 1; i + j < args.length; j++) {
+                    String f = args[i + j];
+                    filelist.add(f);
+                }
+                ;
+            }
+            i++;
+        }
+
+        // primeira coisa criar o socket
+        String serverAdress = host[0];
+        int socket = Integer.parseInt(host[1]);
+        Socket echoSocket = new Socket(serverAdress, socket);
+
+        // criar strings object
+        ObjectInputStream in = new ObjectInputStream(echoSocket.getInputStream());
+        ObjectOutputStream out = new ObjectOutputStream(echoSocket.getOutputStream());
+
+        // e um pdf
+        File myF = new File("pdf.pdf");
+        // tamanho do ficheiro
+        long size = myF.length();
+        out.write((int) size);
+
+        byte buffer[] = new byte[1024];
+        int n;
+        // ler do ficheiro / abrir
+        FileInputStream fStream = new FileInputStream(myF);
+
+        // enquanto ler do ficheiro e enviar p o socket
+        while ((n = fStream.read(buffer, 0, 1024)) > 0) {
+            // dizer q quero escrever p o socket
+            out.write(buffer, 0, n);
+        }
+        fStream.close();
+
+        out.close();
+        in.close();
+        echoSocket.close();
+    }
 
 }
--- src/myCloudServer/myCloudServer.java
+++ src/myCloudServer/myCloudServer.java
@@ -7,45 +7,46 @@ import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-import myServerEX2.ServerThread;
-
 public class myCloudServer {
-	
+
 	public static void main(String[] args) {
+		/**
+		 * if (args.length != 1) {
+		 * System.err.println("Usage: java myCloudServer port");
+		 * System.exit(1);
+		 * }
+		 */
 		System.out.println("servidor: main");
 		int portNumber = 3030;
-		//Integer.parseInt(args[0]); 
-		myCloudServer server = new myCloudServer();		
+		// Integer.parseInt(args[0]);
+		myCloudServer server = new myCloudServer();
 		server.startServer(portNumber);
-		
+
 	}
-	
-	public void startServer (int port){
-		
-		
+
+	public void startServer(int port) {
+
 		ServerSocket sSoc = null;
-        
+
 		try {
 			sSoc = new ServerSocket(port);
 		} catch (IOException e) {
 			System.err.println(e.getMessage());
 			System.exit(-1);
 		}
-         
-		while(true) {
+
+		while (true) {
 			try {
 				Socket inSoc = sSoc.accept();
 				ServerThread newServerThread = new ServerThread(inSoc);
 				newServerThread.start();
-		    }
-		    catch (IOException e) {
-		        e.printStackTrace();
-		    }
-		    
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
 		}
 	}
-	
-	
+
 	class ServerThread extends Thread {
 
 		private Socket socket = null;
@@ -54,50 +55,38 @@ public class myCloudServer {
 			socket = inSoc;
 			System.out.println("thread do server para cada cliente");
 		}
- 
-		public void run(){
-			
+
+		public void run() {
+
 			try {
-				//cria as duas strings
+				// cria as duas strings
 				ObjectOutputStream outStream = new ObjectOutputStream(socket.getOutputStream());
 				ObjectInputStream inStream = new ObjectInputStream(socket.getInputStream());
-				
-				String user = null;
-				String passwd = null;
-			
-				try { //le 2 objetos string do objeto
-					user = (String)inStream.readObject();
-					passwd = (String)inStream.readObject();
-					System.out.println("thread: depois de receber a password e o user");
-				}catch (ClassNotFoundException e1) { 
-					e1.printStackTrace();
-				}
- 			
-				
-				//novo para receber file
+
+				// AQUIIII
+				// novo para receber file
 				long size = inStream.readLong();
-				//le do socket e escreve para ficheiro
-				//crio o ficheito
+				// le do socket e escreve para ficheiro
+				// crio o ficheito
 				FileOutputStream novo = new FileOutputStream("cc.pdf", false);
 				int n;
-				//o nosso buffer so aguenta 1024
+				// o nosso buffer so aguenta 1024
 				byte buffer[] = new byte[1024];
-				//enquanto nao receber tudo 
-				while (size>0) {
-					//le
-					//se size maior q 1024 le so 1024, se nao le tudo e converte de long para int
+				// enquanto nao receber tudo
+				while (size > 0) {
+					// le
+					// se size maior q 1024 le so 1024, se nao le tudo e converte de long para int
 
-					n = inStream.read(buffer, 0, (int) (size>1024?1024:size));
-					
-					
-					//escreve
+					n = inStream.read(buffer, 0, (int) (size > 1024 ? 1024 : size));
+
+					// escreve
 					novo.write(buffer, 0, n);
-					size -=n;
+					size -= n;
 				}
 
 				outStream.close();
 				inStream.close();
- 			
+
 				socket.close();
 
 			} catch (IOException e) {
